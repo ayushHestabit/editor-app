534358fe2953f86f34ed4a992836a8eea38dfd43
Vijay Pratap <vijay.singh@hestabit.in>
2024-11-07 20:19:55 +0530

    Full Screen

--- electron.js
+++ electron.js
@@ -1,19 +1,23 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 const START_URL = 'http://localhost:8080';
 
 const createWindow = async () => {
 	const isDev = (await import('electron-is-dev')).default;
 
+	// Get the primary display's work area size
+	const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
 	let mainWindow = new BrowserWindow({
 		title: "Margent",
-		width: 1200,
-		height: 800,
+		width: width,  // Set the window width to the display's width
+		height: height, // Set the window height to the display's height
 		icon: '',
 		webPreferences: {
 			nodeIntegration: false,
 		},
 	});
+
 	const startUrl = isDev
 		? START_URL
 		: new URL(`file://${path.join(__dirname, '/../build/index.html')}`).toString();
